de5f8e1f953d4b5e376cfb11b1557bde64614bd7
Eirian-Ta <trang.ta911@gmail.com>
2024-03-14 12:46:19 -0400

    Fix contact form

--- app/ui/Contact/ContactForm.tsx
+++ app/ui/Contact/ContactForm.tsx
@@ -88,9 +88,8 @@ const ContactFrom = () => {
           Message
         </label>
         <textarea
-          rows="4"
+          rows={4}
           maxLength={900}
-          type="text"
           className="rounded-md border focus:outline-none py-2 mt-2 px-1 mx-4 focus:ring-2 focus:border-none ring-blue-500"
           name="message"
           required
