b35b7b540066501fbb67fc9c06adc2692a367b25
Eirian-Ta <trang.ta911@gmail.com>
2024-03-28 17:05:41 -0400

    Add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 // app/layout.jsx
 
+import { GoogleAnalytics } from "@next/third-parties/google";
+import dotenv from "dotenv";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./ui/global.css";
 import { ThemeProvider } from "./ui/HeadNav/ThemeProvider";
+
+// Load variables from .env file
+dotenv.config();
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +27,9 @@ export default function RootLayout({
       <head />
       {/* <body className="bg-white dark:bg-black min-h-[100dvh]"> */}
       <body>
-      < ThemeProvider attribute="class">{children}</ThemeProvider>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || ""} />
     </html>
   );
 }
